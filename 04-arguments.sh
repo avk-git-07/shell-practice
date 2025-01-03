103da563aadb4fdd7fe1070f0e162f86b7e5a002
vamshi <asurivamshi@gmail.com>
2025-01-03 10:37:04 +0530

    added 4th file of Arguments

---
+++ 04-arguments.sh
@@ -0,0 +1,15 @@
+#!/git/bash
+
+# in the above line(shebang) the # will not be treated as a comment and in the rest of the code it will be treated as comment.
+
+Person1=$1 # no gap between variable, equals to sign, and value
+Person2=$2 # no gap between variable, equals to sign, and value
+
+echo "$Person1: Hello $Person2!! how are you !!!"
+echo "${Person2}: Hello ${Person1} !! I am fine !! how about you !!!"
+echo "${Person1}: I am doing good ${Person2} !! what's up, did you complete the task !!?"
+echo "$Person2: Yeah, its almost done $Person1,  little patch work is there, it will be completed in an hour !!!" 
+echo "$Person1: Good $Person2, let me know once it is done !!"
+echo "$Person2: Sure $Person1, I will !!!"
+
+
