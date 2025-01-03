6906d509e35663bb04d492c3d7bf0f8e6e7166b6
vamshi <asurivamshi@gmail.com>
2025-01-03 09:43:31 +0530

    did the changes in the names

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,12 +2,12 @@
 
 # in the above line(shebang) the # will not be treated as a comment and in the rest of the code it will be treated as comment.
 
-Person1=Mohandas # no gap between variable, equals to sign, and value
-Person2=Karamchand # no gap between variable, equals to sign, and value
+Person1=Karan # no gap between variable, equals to sign, and value
+Person2=Arjun # no gap between variable, equals to sign, and value
 
 echo "$Person1: Hello $Person2!! how are you !!!"
-echo "$Person2: Hello $Person1 !! I am fine !! how about you !!!"
-echo "$Person1: I am doing good $Person2 !! what's up, did you complete the task !!?"
+echo "${Person2}: Hello ${Person1} !! I am fine !! how about you !!!"
+echo "${Person1}: I am doing good ${Person2} !! what's up, did you complete the task !!?"
 echo "$Person2: Yeah, its almost done $Person1,  little patch work is there, it will be completed in an hour !!!" 
 echo "$Person1: Good $Person2, let me know once it is done !!"
 echo "$Person2: Sure $Person1, I will !!!"
