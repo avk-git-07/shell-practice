783d1b66934b536897452ddfca5337fe30a7bc18
vamshi <asurivamshi@gmail.com>
2025-01-03 11:20:23 +0530

    updated 5th file

--- 05-var-args.sh
+++ 05-var-args.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 echo "Please enter your username"
-read var1
+read var1 # takes input in to var1 variable
 echo "Username entered is : $var1"
+
+echo "Please enter your username"
+read -s var2 # takes input in to var2 variable and "-s" will hide the entered text
+echo "Username entered is : $var2"
+
