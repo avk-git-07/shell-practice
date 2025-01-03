0244349150948b46d68010d2b253bb2411cc2146
vamshi <asurivamshi@gmail.com>
2025-01-03 08:41:16 +0530

    Removed the conversations from this file

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,11 +1,5 @@
 #!/git/bash
 
 # in the above line(shebang) the # will not be treated as a comment and in the rest of the code it will be treated as comment.
-echo "John: Hello Peter!! how are you !!!"
-echo "Peter: Hello john !! I am fine !! how about you !!!"
-echo "John: I am doing good peter !! what's up, did you complete the task !!?"
-echo "Peter: Yeah, its almost done,  little patch work is there, it will be completed in an hour !!!" 
-echo "John: Good, let me know once it is done !!"
-echo "Peter: Sure john, I will !!!"
-
+echo "Hello World"
 
