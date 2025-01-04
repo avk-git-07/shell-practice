6fda75648ce0ea369976d4471cd602bb66e1a2ca
vamshi <asurivamshi@gmail.com>
2025-01-04 05:42:44 +0530

    Added 9th file special variables

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -3,3 +3,7 @@
 echo "All the variables passed to the script: $@" # we can use $* also to get all.
 echo "Number of variables passed : $#"
 echo "Script Name: $0"
+echo "Current Working Directory: $PWD"
+echo "Home Directory of current user: $HOME"
+
+
