78352bcb36d1e34c61d2af877d9e401c5bb20a82
vamshi <asurivamshi@gmail.com>
2025-01-04 06:42:16 +0530

    Updated 10th file special Conditions

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# echo "Please enter a number"
-# read n1
+echo "Please enter a number"
+read n1
 
-# if [ $n1 -gt 10 ] # gap should be there
-# then
-#     echo "your entered number $n1 is greater than 10"
-# else
-#     echo "your entered number $n1 is less than 10"
-# fi
-
-n2=20
-if [ $n2 -gt 10 ] # gap should be there
+if [ $n1 -gt 10 ] # gap should be there
 then
-    echo "the number $n2 is greater than 10"
+    echo "your entered number $n1 is greater than 10"
 else
-    echo "the number $n2 is less than 10"
+    echo "your entered number $n1 is less than 10"
 fi
+
+# n2=20
+# if [ $n2 -gt 10 ] # gap should be there
+# then
+#     echo "the number $n2 is greater than 10"
+# else
+#     echo "the number $n2 is less than 10"
+# fi
