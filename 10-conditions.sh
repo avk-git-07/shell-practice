3d3ac861a6bc140a604e2a67e53547b4c1548c1a
vamshi <asurivamshi@gmail.com>
2025-01-04 06:15:56 +0530

    Added 10th file special Conditions

---
+++ 10-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter a number"
+read n1
+
+if ($n1 > 10) {
+    echo "your entered number $n1 is greater than 10"
+}
+else {
+    echo "your entered number $n1 is less than 10"
+}
