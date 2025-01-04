abfe917ef6f8d9d8a6c3eb23c49b8b89fd810dc2
vamshi <asurivamshi@gmail.com>
2025-01-05 00:14:11 +0530

    Added 13th file colors

---
+++ 13-colors.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# dnf install git -y
+USERID=$(id -u)
+# echo "User id is : $USERID"
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2.. is \e[31m failed.."
+        exit 1
+    else 
+        echo -e "$2.. is \e[32m success.."
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run the script with root user"
+    exit 1
+fi
+
+
+dnf list installed git
+if [ $? -ne 0 ]
+then
+    echo "The git is not yet installed, let us install it."
+    dnf install git -y
+    VALIDATE $? "Git Installation"
+else
+    echo -e "\e{32m The git is installed, nothing to do..."
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+    echo "Mysql installation is not done, let us install it.."
+    dnf install mysql -y
+    VALIDATE $? "Mysql Installation"
+else 
+    echo -e "\e[32m Mysql is already installed, nothing to do.."
+fi
+
+
