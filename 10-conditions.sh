aa45fec9f5b954353613ed264c2b72e7af7fc1e3
vamshi <asurivamshi@gmail.com>
2025-01-04 06:30:01 +0530

    Updated 10th file special Conditions

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-echo "Please enter a number"
-read n1
+# echo "Please enter a number"
+# read n1
 
-if [$n1 -gt 10]
+# if [$n1 -gt 10]
+# then
+#     echo "your entered number $n1 is greater than 10"
+# else
+#     echo "your entered number $n1 is less than 10"
+# fi
+
+n2 = 20
+if [$n2 -gt 10]
 then
-    echo "your entered number $n1 is greater than 10"
+    echo "your entered number $n2 is greater than 10"
 else
-    echo "your entered number $n1 is less than 10"
+    echo "your entered number $n2 is less than 10"
 fi
