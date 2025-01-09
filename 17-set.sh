1003497c76551c563181bea018422c1e3c22b41f
vamshi <asurivamshi@gmail.com>
2025-01-09 19:47:20 +0530

    Updated 17 file set

--- 17-set.sh
+++ 17-set.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-set -ex # Setting the automatic exit, if we get error. Here -e for exit, x for debug
+set -ex # Setting the automatic exit, if we get error. Here -e for exit and -ex for debug
+
+failure(){
+    echo "Failed at $1:$2"
+}
+
+trap 'failure "${LINENO}" "$BASH_COMMAND"' ERR # ERR is the error signal
 echo "Hi Good Morning"
 echoo "Hi very good morning"
 echo "Good Evening"
