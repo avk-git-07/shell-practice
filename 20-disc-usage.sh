1fa6e3e8fde68d5fde7311680286ceed90706c7f
vamshi <asurivamshi@gmail.com>
2025-01-21 14:59:51 +0530

    Added 20-disc-usage.sh

--- 20-disc-usage.sh
+++ 20-disc-usage.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-
+R="\e[31m"
+N="\e[0m"
 disc_usage=$(df -hT | grep xfs)
 disc_threshold=5
 
 while IFS= read -r line; do
-    usage=$(echo ${line} | grep xfs | awk -F " " '{print $6}' | cut -d "%" -f1)
-    partition=$(echo ${line} | grep xfs | awk -F " " '{print $NF}')
+    usage=$(echo "${line}" | grep xfs | awk -F " " '{print $6}' | cut -d "%" -f1)
+    partition=$(echo "${line}" | grep xfs | awk -F " " '{print $NF}')
     if [ "${usage}" -ge "${disc_threshold}" ]
     then 
-        echo "${partition} is showing ${usage}%, which is more than the threshold ${disc_threshold}%, please check..."  
+        echo -e "${R}${partition} is showing ${usage}%, which is more than the threshold percentage ${disc_threshold}, please check...${N}"  
     fi 
 done <<< "${disc_usage}"
 
